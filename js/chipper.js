9db37f37df36614d659b4d7f9d0b470cadabe7fa
Antonio Jesús Padial <antonio@spadial.com>
2016-09-27 23:28:54 +0200

    Added school table component and entities

--- js/chipper.js
+++ js/chipper.js
@@ -8,3 +8,43 @@ Crafty.defineScene('mainScene', function() {
 });
 
 Crafty.enterScene('mainScene');
+
+Crafty.c('Table', {
+  _size: 1,
+  events: {
+    'Change': function(data) {
+      this._cleanTables();
+      this._size = data.size;
+      this.x = data.x;
+      this.y = data.y;
+      this._setTables();
+    },
+  },
+  init: function() {
+    this._entities = [];
+    this._setTables(this._size);
+  },
+  _setTables: function(s) {
+    this._entities = [];
+    for (var i = 0; i < this._size; i++) {
+      this._entities.push(Crafty.e('2D, Canvas, Image')
+        .attr({x: this.x + i * 45, y: this.y})
+        .image('images/schooltable.png')
+      );
+    }
+  },
+  _cleanTables: function() {
+    for (var i = 0; i < this._size; i++) {
+      this._entities[i].destroy();
+    }
+  }
+});
+
+Crafty.e('2D, Canvas, Table')
+  .attr({x: 82, y: 55, size: 3});
+Crafty.e('2D, Canvas, Table')
+  .attr({x: 54, y: 93, size: 4});
+Crafty.e('2D, Canvas, Table')
+  .attr({x: 30, y: 131, size: 5}); 
+Crafty.e('2D, Canvas, Table')
+  .attr({x: 5, y: 169, size: 6});
