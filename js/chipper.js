8de69d6deab5a3555140df61991f6977c197e0a5
Antonio Jesús Padial <antonio@spadial.com>
2016-09-27 22:36:13 +0200

    Scale x3

--- js/chipper.js
+++ js/chipper.js
@@ -1,9 +1,10 @@
-Crafty.init(320, 200, document.getElementById('game'));
+Crafty.init(960, 600, document.getElementById('game'));
 
 Crafty.defineScene('mainScene', function() {
   Crafty.pixelart(true);
   Crafty.e('2D, Canvas, Image')
     .image('images/background.png');
+  Crafty.viewport.scale(3);
 });
 
 Crafty.enterScene('mainScene');
