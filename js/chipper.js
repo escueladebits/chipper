d05aea14c5b28e2dacb1c397e4e47b5d0efef49e
Antonio Jesús Padial <antonio@spadial.com>
2016-09-27 22:34:31 +0200

    Created basic scene

--- js/chipper.js
+++ js/chipper.js
@@ -1,1 +1,9 @@
 Crafty.init(320, 200, document.getElementById('game'));
+
+Crafty.defineScene('mainScene', function() {
+  Crafty.pixelart(true);
+  Crafty.e('2D, Canvas, Image')
+    .image('images/background.png');
+});
+
+Crafty.enterScene('mainScene');
