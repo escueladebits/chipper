ccbb5161e7b98b68cf6e2cda6a5da72274b8663a
Antonio Jesús Padial <antonio@spadial.com>
2016-09-28 17:59:38 +0200

    Minor text formating

--- js/chipper.js
+++ js/chipper.js
@@ -6,15 +6,14 @@ Crafty.defineScene('mainScene', function() {
     .image('images/background.png');
   Crafty.viewport.scale(3);
 
-
-Crafty.e('2D, Canvas, Table')
-  .attr({x: 82, y: 55, size: 3});
-Crafty.e('2D, Canvas, Table')
-  .attr({x: 54, y: 93, size: 4});
-Crafty.e('2D, Canvas, Table')
-  .attr({x: 30, y: 131, size: 5}); 
-Crafty.e('2D, Canvas, Table')
-  .attr({x: 5, y: 169, size: 6});
+  Crafty.e('2D, Canvas, Table')
+    .attr({x: 82, y: 55, size: 3});
+  Crafty.e('2D, Canvas, Table')
+    .attr({x: 54, y: 93, size: 4});
+  Crafty.e('2D, Canvas, Table')
+    .attr({x: 30, y: 131, size: 5}); 
+  Crafty.e('2D, Canvas, Table')
+    .attr({x: 5, y: 169, size: 6});
 });
 
 Crafty.enterScene('mainScene');
