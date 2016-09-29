03d35be97e6771bf4853d6c2dbe52fa61f186ba1
Antonio Jesús Padial <antonio@spadial.com>
2016-09-29 22:58:56 +0200

    Refactor Table to leverage Crafty's children features

--- js/components/Table.js
+++ js/components/Table.js
@@ -2,29 +2,29 @@ Crafty.c('Table', {
   _size: 1,
   events: {
     'Change': function(data) {
-      this._cleanTables();
-      this._size = data.size;
+      var rebuild = this.size !== data.size;
       this.x = data.x;
       this.y = data.y;
-      this._setTables();
+      this._size = data.size;
+      if (rebuild) {
+        for (var c of this._children) {
+          this.detach(c).destroy();
+        }
+        this._createChildren();
+      }
     },
   },
+  required: '2D, Canvas, Image',
   init: function() {
-    this._entities = [];
-    this._setTables(this._size);
+    this.image('images/schooltable.png');
+    this._createChildren();
   },
-  _setTables: function(s) {
-    this._entities = [];
-    for (var i = 0; i < this._size; i++) {
-      this._entities.push(Crafty.e('2D, Canvas, Image')
-        .attr({x: this.x + i * 45, y: this.y})
+  _createChildren: function() {
+    for (var i = 1; i < this._size; i++) {
+      this.attach(Crafty.e('2D, Canvas, Image')
+        .attr({x: this._x + i * 45, y: this._y})
         .image('images/schooltable.png')
       );
     }
   },
-  _cleanTables: function() {
-    for (var i = 0; i < this._size; i++) {
-      this._entities[i].destroy();
-    }
-  }
 });
